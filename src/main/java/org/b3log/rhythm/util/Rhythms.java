d6e6f196a3de18e9b6d1b1fc6365b971b8d361b7
Liang Ding <dl88250@gmail.com>
2016-08-19 10:15:24 +0800

    Release Sym 1.5.0

--- src/main/java/org/b3log/rhythm/util/Rhythms.java
+++ src/main/java/org/b3log/rhythm/util/Rhythms.java
@@ -24,7 +24,7 @@ import org.b3log.latke.util.Strings;
  * Rhythm utilities.
  *
  * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
- * @version 1.5.1.23, Aug 10, 2016
+ * @version 1.6.1.23, Aug 19, 2016
  * @since 0.1.4
  */
 public final class Rhythms {
@@ -128,6 +128,7 @@ public final class Rhythms {
             add("1.0.0");
             add("1.3.0");
             add("1.4.0");
+            add("1.5.0");
         }
     };
 
@@ -140,7 +141,7 @@ public final class Rhythms {
     /**
      * The latest development Symphony version.
      */
-    public static final String SNAPSHOT_SYMPHONY_VERSION = "1.5.0";
+    public static final String SNAPSHOT_SYMPHONY_VERSION = "1.6.0";
 
     /**
      * Checks whether the specified client name is valid.
