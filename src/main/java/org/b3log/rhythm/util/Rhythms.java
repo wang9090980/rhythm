4b423653e6eafaa566609ba0dc8c5e7ed18e78eb
Liang Ding <DL88250@gmail.com>
2016-09-08 01:17:16 +0800

    发布 - Solo 1.6.0

--- src/main/java/org/b3log/rhythm/util/Rhythms.java
+++ src/main/java/org/b3log/rhythm/util/Rhythms.java
@@ -24,7 +24,7 @@ import org.b3log.latke.util.Strings;
  * Rhythm utilities.
  *
  * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
- * @version 1.6.1.24, Aug 31, 2016
+ * @version 1.6.1.25, Sep 8, 2016
  * @since 0.1.4
  */
 public final class Rhythms {
@@ -79,6 +79,7 @@ public final class Rhythms {
             add("1.3.0");
             add("1.4.0");
             add("1.5.0");
+            add("1.6.0");
         }
     };
 
@@ -91,7 +92,7 @@ public final class Rhythms {
     /**
      * The latest development Solo version.
      */
-    public static final String SNAPSHOT_SOLO_VERSION = "1.6.0";
+    public static final String SNAPSHOT_SOLO_VERSION = "1.7.0";
 
     /**
      * Released Wide versions.
